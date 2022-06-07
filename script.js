ee674a174785c2a6a3779c8dee974bb0a5efec26
Mainulhasan05 <mdrifatbd5@gmail.com>
2022-06-07 18:51:20 +0600

    msg

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const loadImg=async()=>{
     var img=document.getElementById('image')
-    var id=document.getElementById('inputid')
-    img.src="https://portal.ewubd.edu/Documents/StudentProfile/2019-2-60-037.jpg"
+    var id=document.getElementById('inputid').value
+    img.src=`https://portal.ewubd.edu/Documents/StudentProfile/`+id+".jpg"
+    
 }
